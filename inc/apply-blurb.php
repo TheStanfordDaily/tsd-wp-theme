bef7398f1e7678996b52b6f3f156e12a06a9061b
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-09-25 17:20:41 +0000

    add apply header, hide donate header

--- inc/apply-blurb.php
+++ inc/apply-blurb.php
@@ -3,7 +3,7 @@
 		<div class="row text-center">
             <div class="col-12">
                 <a href="/join-the-daily/?src=header">
-                    <img src="https://www.stanforddaily.com/wp-content/uploads/2019/04/SPRING-FB-COVER.png" style="max-height: 40vh;"><br>
+                    <img src="https://www.stanforddaily.com/wp-content/uploads/2019/09/70956515_2506558542907707_2665576761118425088_n.jpg" style="max-height: 30vh;"><br>
                     <button class="tsd-button" type="button">Apply Now</button>
                 </a>
             </div>
